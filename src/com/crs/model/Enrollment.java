835bc68f0b1701cd3ac31e8894fc0191d00f97d0
Isuri003 <isuriwijesinghe540@gmail.com>
2025-02-26 22:56:42 +0530

    Update Enrollment.java

--- src/com/crs/model/Enrollment.java
+++ src/com/crs/model/Enrollment.java
@@ -1,1 +1,10 @@
+public class Enrollment {
+    private int enrollmentId;
+    private Student student;
+    private Course course;
+    private String status;
+    private String semester;
+
+    
+}
 
