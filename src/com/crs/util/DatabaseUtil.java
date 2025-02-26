d3dad10892a7dc15247b963f5def68fa56879908
Isuri003 <isuriwijesinghe540@gmail.com>
2025-02-26 23:02:06 +0530

    Update DatabaseUtil.java

--- src/com/crs/util/DatabaseUtil.java
+++ src/com/crs/util/DatabaseUtil.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseUtil {
+
     private static final String URL = "jdbc:mysql://localhost:3306/CRS";
     private static final String USER = "root";
     private static final String PASSWORD = "password";  
