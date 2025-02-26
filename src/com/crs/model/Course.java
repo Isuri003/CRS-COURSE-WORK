9c18192ba7dbd01946a6c3584df0e7572a568813
Isuri003 <isuriwijesinghe540@gmail.com>
2025-02-26 22:54:21 +0530

    Update Course.java

--- src/com/crs/model/Course.java
+++ src/com/crs/model/Course.java
@@ -1,1 +1,11 @@
+public class Course {
+    private int courseId;
+    private String title;
+    private int creditHours;
+    private String department;
+    private int prerequisites;
+    private int maxEnrollment;
+
+    
+}
 
