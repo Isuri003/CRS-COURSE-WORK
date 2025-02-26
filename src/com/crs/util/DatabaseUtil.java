d0da9fcabe6e42bcf531cbe5d439271296c2db39
Isuri003 <isuriwijesinghe540@gmail.com>
2025-02-26 22:55:10 +0530

    Update DatabaseUtil.java

--- src/com/crs/util/DatabaseUtil.java
+++ src/com/crs/util/DatabaseUtil.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/CRS";
     private static final String USER = "root";
-    private static final String PASSWORD = "password";  // Replace with your MySQL password
+    private static final String PASSWORD = "password";  
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
