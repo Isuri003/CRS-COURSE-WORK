f60b6193cc061fbb990bbe2f41dc49065bdf8a8c
Isuri003 <isuriwijesinghe540@gmail.com>
2025-02-26 23:04:19 +0530

    Update Grade.java

--- src/com/crs/model/Grade.java
+++ src/com/crs/model/Grade.java
@@ -1,1 +1,31 @@
+public class Grade {
+    private int gradeId;
+    private Enrollment enrollment;
+    private String grade;
+
+    // Getters and setters
+    public int getGradeId() {
+        return gradeId;
+    }
+
+    public void setGradeId(int gradeId) {
+        this.gradeId = gradeId;
+    }
+
+    public Enrollment getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(Enrollment enrollment) {
+        this.enrollment = enrollment;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+}
 
