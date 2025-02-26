7804b6d6e01a69332ca59c6619628c3eb0eb469c
Isuri003 <isuriwijesinghe540@gmail.com>
2025-02-26 23:03:01 +0530

    Update Student.java

--- src/com/crs/model/Student.java
+++ src/com/crs/model/Student.java
@@ -6,6 +6,52 @@ public class Student {
     private int year;
     private String contactInfo;
 
-   
-}
+    // Getters and setters
+    public int getStudentId() {
+        return studentId;
+    }
 
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getProgramOfStudy() {
+        return programOfStudy;
+    }
+
+    public void setProgramOfStudy(String programOfStudy) {
+        this.programOfStudy = programOfStudy;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+}
