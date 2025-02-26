8e2dce9d127edb09622fe88362aeadf1be12cd7a
Isuri003 <isuriwijesinghe540@gmail.com>
2025-02-26 22:56:00 +0530

    Update Student.java

--- src/com/crs/model/Student.java
+++ src/com/crs/model/Student.java
@@ -1,1 +1,11 @@
+public class Student {
+    private int studentId;
+    private String name;
+    private String dob;
+    private String programOfStudy;
+    private int year;
+    private String contactInfo;
+
+   
+}
 
