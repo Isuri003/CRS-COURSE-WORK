985962be3cef8ea12fdb07039eeb921a17183795
Isuri003 <isuriwijesinghe540@gmail.com>
2025-02-26 23:03:52 +0530

    Update Enrollment.java

--- src/com/crs/model/Enrollment.java
+++ src/com/crs/model/Enrollment.java
@@ -5,6 +5,46 @@ public class Enrollment {
     private String status;
     private String semester;
 
-    
+   
+    public int getEnrollmentId() {
+        return enrollmentId;
+    }
+
+    public void setEnrollmentId(int enrollmentId) {
+        this.enrollmentId = enrollmentId;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSemester() {
+        return semester;
+    }
+
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
 }
+
 
