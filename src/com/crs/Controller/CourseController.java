6b5021a1c9ed78afa172384d823f13809813a829
Isuri003 <isuriwijesinghe540@gmail.com>
2025-02-26 23:05:38 +0530

    Update CourseController.java

--- src/com/crs/Controller/CourseController.java
+++ src/com/crs/Controller/CourseController.java
@@ -1,1 +1,25 @@
+package com.crs.controller;
+
+import com.crs.model.Course;
+import com.crs.service.CourseService;
+
+public class CourseController {
+
+    private CourseService courseService;
+
+    public CourseController() {
+        this.courseService = new CourseService();
+    }
+
+    public void addCourse(String title, int creditHours, String department, int prerequisites, int maxEnrollment) {
+        Course course = new Course();
+        course.setTitle(title);
+        course.setCreditHours(creditHours);
+        course.setDepartment(department);
+        course.setPrerequisites(prerequisites);
+        course.setMaxEnrollment(maxEnrollment);
+
+        courseService.addCourse(course);
+    }
+}
 
