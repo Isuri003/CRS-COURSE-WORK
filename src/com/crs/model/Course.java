66aed400a9eedcd92ea0f1612651c0719741de7c
Isuri003 <isuriwijesinghe540@gmail.com>
2025-02-26 23:01:09 +0530

    Update Course.java

--- src/com/crs/model/Course.java
+++ src/com/crs/model/Course.java
@@ -6,6 +6,53 @@ public class Course {
     private int prerequisites;
     private int maxEnrollment;
 
-    
+    // Getters and setters
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setPrerequisites(int prerequisites) {
+        this.prerequisites = prerequisites;
+    }
+
+    public int getMaxEnrollment() {
+        return maxEnrollment;
+    }
+
+    public void setMaxEnrollment(int maxEnrollment) {
+        this.maxEnrollment = maxEnrollment;
+    }
 }
 
