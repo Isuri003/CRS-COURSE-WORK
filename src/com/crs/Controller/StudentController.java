82c7fd9b5e3c5351d2bcc4a7bd5345442d784f2c
Isuri003 <isuriwijesinghe540@gmail.com>
2025-02-26 23:05:01 +0530

    Update StudentController.java

--- src/com/crs/Controller/StudentController.java
+++ src/com/crs/Controller/StudentController.java
@@ -1,1 +1,25 @@
+package com.crs.controller;
+
+import com.crs.model.Student;
+import com.crs.service.StudentService;
+
+public class StudentController {
+
+    private StudentService studentService;
+
+    public StudentController() {
+        this.studentService = new StudentService();
+    }
+
+    public void registerStudent(String name, String dob, String programOfStudy, int year, String contactInfo) {
+        Student student = new Student();
+        student.setName(name);
+        student.setDob(dob);
+        student.setProgramOfStudy(programOfStudy);
+        student.setYear(year);
+        student.setContactInfo(contactInfo);
+        
+        studentService.addStudent(student);
+    }
+}
 
